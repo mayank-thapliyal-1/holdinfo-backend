afd6fdaddb614b2248cc5e873d90506390c39c53
mayank-thapliyal-1 <111272849+mayank-thapliyal-1@users.noreply.github.com>
2023-09-19 19:47:58 +0530

    feat: add database logic

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
 const app = express();
-import cors from 'cors';
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const PORT = process.env.PORT || 4000;
+
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
 
 app.use(cors());
 
-// Define a route to fetch and process data
-app.get('/fetchData', async (req, res) => {
+app.get("/insertIntoDB", async (req, res) => {
   try {
-    // Fetch data using Axios
-    const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
+    const response = await axios.get("https://api.wazirx.com/api/v2/tickers");
     const data = response.data;
 
-    const top10Tickers = Object.values(data).map(ticker => {
-        return {
-          name: ticker.name,
-          last: parseFloat(ticker.last),
-          buy: parseFloat(ticker.buy),
-          sell: parseFloat(ticker.sell),
-          volume: parseFloat(ticker.volume),
-          base_unit: ticker.base_unit
-        };
-      });
-  
-      // Sort the top 10 by buy value in descending order
-      const sortedTop10Tickers = top10Tickers.sort((a, b) => b.buy - a.buy).slice(0, 10);
+    const top10Tickers = Object.values(data).map((ticker) => {
+      return {
+        name: ticker.name,
+        last: parseFloat(ticker.last),
+        buy: parseFloat(ticker.buy),
+        sell: parseFloat(ticker.sell),
+        volume: parseFloat(ticker.volume),
+        base_unit: ticker.base_unit,
+      };
+    });
 
-    res.json(sortedTop10Tickers);
+    // Sort the top 10 by buy value in descending order
+    const sortedTop10Tickers = top10Tickers
+      .sort((a, b) => b.buy - a.buy)
+      .slice(0, 10);
+
+    const client = await pool.connect();
+    await client.query("DELETE FROM crypto");
+    for (const ticker of sortedTop10Tickers) {
+      const { name, last, buy, sell, volume, base_unit } = ticker;
+
+      await client.query(
+        "INSERT INTO crypto (name, last, buy, sell, volume, base_unit) VALUES ($1, $2, $3, $4, $5, $6)",
+        [name, last, buy, sell, volume, base_unit]
+      );
+    }
+    client.release();
+    res.json({ message: "Data inserted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).send('An error occurred while fetching or processing data.');
+    res
+      .status(500)
+      .send("An error occurred while inserting data into PostgreSQL.");
+  }
+});
+
+// Define a route to fetch and process data
+app.get("/fetchData", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM crypto");
+    const data = result.rows;
+    client.release();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("An error occurred while fetching data from PostgreSQL.");
   }
 });
 
